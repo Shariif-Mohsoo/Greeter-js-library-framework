77ac40aeda714856b434ea14bc18e540cb875273
Shariif-Rajpoot <mr8781256@gmail.com>
2024-10-14 07:06:29 +0500

    Testing Code For Greeter

--- app.js
+++ app.js
@@ -1,2 +1,11 @@
 const g = G$("Mohsin", "Rajpoot");
-console.log(g);
+g.greet().setLang("es").greet(true);
+// console.log(g);
+// lets use our object on the click of the login button
+$("#login").click(function () {
+  // create a new 'Greetr' object("Lets pretend we know the name for the login")
+  const loginGrtr = G$("MUHAMMAD", "MOHSIN");
+  $("#logindiv").hide();
+
+  loginGrtr.setLang($("#lang").val()).HTMLGreeting("#greeting", true).log();
+});
